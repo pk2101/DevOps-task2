9c9aaa303382b0806872634af1b0b6c4b9dad163
pk2101 <64659553+pk2101@users.noreply.github.com>
2020-07-20 23:51:44 +0530

    Update and rename index.php.txt to index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <body bgcolor='orange'>
 <pre>
 <?php
- print("!!! Welcome to Web-Server Testing Site !!!")
+ print("!!! Welcome to Web-Server Testing Site !!!");
  print ("\n");
  print `ifconfig`;
 ?>
